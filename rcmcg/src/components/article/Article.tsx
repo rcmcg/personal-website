d62e5eb2cbae320ef9a0907712803e348b5dd8f6
reece.mcgowan <reece.mcgowan@hotmail.com>
2025-12-24 11:17:14 -0800

    fetch lorem from api

--- rcmcg/src/components/article/Article.tsx
+++ rcmcg/src/components/article/Article.tsx
@@ -1,18 +1,36 @@
+import {useEffect, useState} from "react";
+
 interface IArticle {
   title: string
   date: string
-  article: string
 }
 
-function Article( {title, date, article}: IArticle ) {
+function Article( {title, date}: IArticle ) {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [article, setArticle] = useState<string>("");
+
+  useEffect(() => {
+    fetch('https://lorem-api.com/api/article/foo')
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => {
+        setArticle(data['content']);
+        setIsLoading(false);
+      })
+  }, []);
 
   return (
     <div className={"_article"}>
       <h3>{title}</h3>
       <div className={"article-date"}>{date}</div>
-      <article>
-        {article}
-      </article>
+      {!isLoading &&
+          <article>
+            {article}
+          </article>
+      }
+
     </div>
   )
 }
