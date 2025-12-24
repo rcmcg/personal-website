7390ea1b828205f7bb60c8f9af71d62c03a258dc
reece.mcgowan <reece.mcgowan@hotmail.com>
2025-12-24 08:39:24 -0800

    add navigation part of header

--- rcmcg/src/App.tsx
+++ rcmcg/src/App.tsx
@@ -3,12 +3,18 @@ function Header() {
 
   return (
     <>
-        <div className={"header"}>
-          <h1>rcmcg</h1>
-          <div className={"header-item"}>vancouver</div>
-          <div className={"header-item"}>visier</div>
-          <div className={"header-item"}>sfu</div>
-        </div>
+      <div className={"header header-first-line"}>
+        <h1>rcmcg</h1>
+        <div className={"header-item"}>vancouver</div>
+        <div className={"header-item"}>visier</div>
+        <div className={"header-item"}>sfu</div>
+      </div>
+      <div className={"header header-second-line"}>
+        <div className={"header-item"}>home</div>
+        <div className={"header-item"}>projects</div>
+        <div className={"header-item"}>blog</div>
+        <div className={"header-item"}>about</div>
+      </div>
     </>
   )
 }
