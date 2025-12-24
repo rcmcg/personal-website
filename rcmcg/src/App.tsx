7a5d45151f30734f0ee2fc4019b95550c31009d4
reece.mcgowan <reece.mcgowan@hotmail.com>
2025-12-23 19:24:08 -0800

    first line of header

--- rcmcg/src/App.tsx
+++ rcmcg/src/App.tsx
@@ -1,9 +1,23 @@
 
+function Header() {
+
+  return (
+    <>
+        <div className={"header"}>
+          <h1>rcmcg</h1>
+          <div className={"header-item"}>vancouver</div>
+          <div className={"header-item"}>visier</div>
+          <div className={"header-item"}>sfu</div>
+        </div>
+    </>
+  )
+}
+
 function App() {
 
   return (
     <>
-      <h1>rcmcg</h1>
+      <Header />
     </>
   )
 }
