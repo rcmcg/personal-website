f67423fce05a30e152d8f9c339d1e587a1fd7e22
reece.mcgowan <reece.mcgowan@hotmail.com>
2025-12-24 09:10:23 -0800

    add navigation links, reformatting

---
+++ rcmcg/src/pages/App.tsx
@@ -0,0 +1,25 @@
+import Header from "../components/header/Header.tsx";
+
+
+function Body() {
+  return (
+    <>
+      <div className={"main"}>
+        <h2>WIP</h2>
+        <div>ETA: whenever</div>
+      </div>
+    </>
+  )
+}
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  )
+}
+
+export default App
---
+++ rcmcg/src/pages/Blog.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/header/Header.tsx";
+
+function Blog() {
+  return (
+    <>
+      <Header />
+      <h2>Thoughts</h2>
+    </>
+  )
+}
+
+export default Blog
